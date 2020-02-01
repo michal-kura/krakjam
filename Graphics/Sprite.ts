253b3d03daa1029173927aaf27b33a0d905ed569
michal-kura <caras84@gmail.com>
2020-02-01 15:42:03 +0100

    1 file modified

--- Graphics/Sprite.ts
+++ Graphics/Sprite.ts
@@ -7,7 +7,7 @@ export class Sprite {
     public size: {width: number, height: number} = {width: 10, height: 1}
   ) { 
     this.image.src = imageUrl;
-    this.image.crossOrigin="anonymous";
+    this.image.crossOrigin="anonymous";;
     this.image.onload = () => {
       this.isReady = true;
     };
